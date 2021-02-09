194e970afce3197670db9dd55de293d634e6f322
Bruno Antunes Miranda <bruno.miranda@localiza.com>
2021-02-08 21:17:17 -0300

    First Commit - Projeto inicial

---
+++ el.localiza.reservas.mvc.netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace el.localiza.reservas.mvc.netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
