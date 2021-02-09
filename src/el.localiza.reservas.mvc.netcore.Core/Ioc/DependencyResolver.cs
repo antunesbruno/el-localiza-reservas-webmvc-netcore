4bd29d7d5918f7fa4906b73cb6364f500dd80113
Bruno Antunes Miranda <bruno.miranda@localiza.com>
2021-02-09 10:48:08 -0300

    Criação das camadas de apoio e inserido alguns pacotes nuget.

---
+++ src/el.localiza.reservas.mvc.netcore.Core/Ioc/DependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using el.localiza.reservas.mvc.netcore.DataSource.Requests;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace el.localiza.reservas.mvc.netcore.Core.Ioc
+{
+    public static class DependencyResolver
+    {
+        public static void AddDependencyResolver(this IServiceCollection services)
+        {
+            RegisterApplications(services);
+            RegisterServices(services);
+            RegisterRefit(services);
+        }
+
+        private static void RegisterApplications(IServiceCollection services)
+        {
+
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+        }
+        private static void RegisterRefit(IServiceCollection services)
+        {
+            //Refit Api Reservas
+            services.AddRefitClient<IRefitReservasApi>()
+                   .ConfigureHttpClient(c =>
+                   {
+                       c.BaseAddress = new Uri(Environment.GetEnvironmentVariable("EL_RESERVAS_REFIT_API_BASE"));
+                       c.DefaultRequestHeaders.Add("Accept", "application/json");
+                   });
+        }      
+    }
+}
--- src/el.localiza.reservas.mvc.netcore.Web/Startup.cs
+++ src/el.localiza.reservas.mvc.netcore.Web/Startup.cs
@@ -1,3 +1,4 @@
+using el.localiza.reservas.mvc.netcore.Core.Ioc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,7 +24,10 @@ namespace el.localiza.reservas.mvc.netcore.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDependencyResolver();
+
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
